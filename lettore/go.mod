a43c9b55e79c44847554ac44ee53122ff0f59896
Ax6 <aaronrusso@hotmail.it>
2023-12-28 16:31:08 +0000

    ook

---
+++ lettore/go.mod
@@ -0,0 +1,3 @@
+module github.com/Ax6/stupid-caldaia/lettore
+
+go 1.21.3
---
+++ lettore/main.go
@@ -0,0 +1,9 @@
+package lettore
+
+import (
+	"github.com/Ax6/stupid-caldaia/controller/graph/model"
+)
+
+func main() {
+	model.Switch
+}
